dbbe6f18ade515cfa4bcb130551e04bcbe7de74c
leihang <1205631051@qq.com>
2022-08-21 15:32:55 +0800

    git测试

---
+++ app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div>
+    <my-header></my-header>
+    <!-- 路由组件 -->
+    <router-view></router-view>
+    <my-footer v-show="$route.meta.show"></my-footer>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/Header'
+import MyFooter from './components/Footer'
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyFooter
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
